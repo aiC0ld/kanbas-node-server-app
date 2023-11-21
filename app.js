9b858deb1efd10d3d8ae6b4f8ea2eb07a6698f58
Xinyue Han <xinyuehan@Vera-2.local>
2023-11-20 19:53:21 -0800

    node

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : process.env.FRONTEND_URL_LOCAL,
+        // origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : process.env.FRONTEND_URL_LOCAL,
+        origin: process.env.FRONTEND_URL,
     }));
 app.use(express.json());
 // AssignmentRoutes(app);
