9665fc9d0c8e155e5f7c165f6651d13d05fb6c71
Xinyue Han <xinyuehan@Vera-2.local>
2023-12-04 18:11:53 -0800

    a6 Mongo

--- app.js
+++ app.js
@@ -6,13 +6,43 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignment/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+const db = mongoose.connection;
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+    console.log(`Database name: ${db.name}`);
+});
+
 const app = express();
 app.use(
     cors({
         credentials: true,
         origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : process.env.FRONTEND_URL_LOCAL,
     }));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(
+    session(sessionOptions)
+);
 app.use(express.json());
+
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app)
